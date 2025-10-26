f75b0796674cefec5d6c0c75f31cc2ad4c1aa571
Ryan Ewane <ryanewane496@gmail.com>
2025-10-26 13:53:38 +0100

    Initial commit - Spring Boot backend

---
+++ src/test/java/com/app/Harvest/HarvestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.Harvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
